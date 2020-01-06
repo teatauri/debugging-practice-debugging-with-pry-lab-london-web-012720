7966e37604088278e944d4501f410793566d2bf6
Lee Bardon <leerbardon@gmail.com>
2020-01-06 16:27:15 +0000

    Done.

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -27,3 +27,5 @@ class Person
     end
   end
 end
+end
+
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,7 +12,9 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  greeting = []
+  crew.each {|crew_member| greeting << "Hello #{crew_member}."}
+  greeting
 end
 
 def engage
